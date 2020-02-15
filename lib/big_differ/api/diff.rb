fc9153a1d85906451e678a729a204ee9dbe02ab8
acroos <roos.austin@gmail.com>
2020-02-15 23:00:56 +0100

    Improve diff parsing for GitHub, add simple diff printing

--- lib/big_differ/api/diff.rb
+++ lib/big_differ/api/diff.rb
@@ -1,19 +1,38 @@
 module BigDiffer::Api
   class Diff
-    FILE_PARSER_REGEX = /\A.*---\s(a)?\/(?<old_file>.*?)\s\+\+\+\sb\/(?<new_file>.*?)\s@@.*@@\s(?<changes>.*)\z/m
+    FILE_PARSER_REGEX = /\A\s*((new\sfile\smode\s*(?<new_file_mode>\d{6}).*)|(.*?(?<old_file_mode>\d{6})))\s*---\s(a)?\/(?<old_filename>.*?)\s\+\+\+\sb\/(?<new_filename>.*?)\s@@\s*\-(?<old_start_line>\d+),(?<old_line_count>\d+)\s*\+(?<new_start_line>\d+),(?<new_line_count>\d+)\s*@@\s(?<changes>.*)\z/m
 
-    attr_accessor :old_file, :new_file, :changes
-    def initialize(old_file, new_file, changes)
-      @old_file = old_file
-      @new_file = new_file
+    attr_accessor :new_mode, :old_mode,
+        :new_filename, :old_filename,
+        :new_start_line, :old_start_line,
+        :new_line_count, :old_line_count,
+        :changes
+    def initialize(new_mode, old_mode, new_filename, old_filename,
+      new_start_line, old_start_line, new_line_count, old_line_count, changes)
+      @new_mode = new_mode
+      @old_mode = old_mode
+      @new_filename = new_filename
+      @old_filename = old_filename
+      @new_start_line = new_start_line
+      @old_start_line = old_start_line
+      @new_line_count = new_line_count
+      @old_line_count = old_line_count
       @changes = changes
+    end
+
+    def is_new?
+      @old_mode.nil? || @old_mode.empty?
     end
 
     def self.parse_git_diff(diff_text)
       file_diffs = diff_text.split(/^diff\s\-\-git\sa\/.*\sb\/.*$/).reject(&:empty?)
       file_diffs.map do |file_diff|
         match = parse(file_diff)
-        Diff.new(match['old_file'], match['new_file'], match['changes'])
+        new_mode = match['new_mode'] rescue nil
+        old_mode = match['old_mode'] rescue nil
+        Diff.new(new_mode, old_mode, match['new_filename'],
+          match['old_filename'], match['new_start_line'], match['old_start_line'],
+          match['new_line_count'], match['old_line_count'], match['changes'])
       end
     end
 
--- lib/big_differ/review.rb
+++ lib/big_differ/review.rb
@@ -1,4 +1,5 @@
 require 'big_differ/api/api'
+require 'colorize'
 
 module BigDiffer
   class Review
@@ -9,8 +10,50 @@ module BigDiffer
     def begin
       diffs = @api.fetch_diffs
       diffs.each do |diff|
-        puts diff.changes
+        print_diff(diff)
       end
+    end
+
+    private
+    def print_diff(diff)
+      print_separator
+      puts "FILE INFO".yellow
+      print_separator
+      print_file_info(diff)
+      print_separator
+      puts "CHANGES".yellow
+      print_separator
+      diff.changes.split("\n").each do |line|
+        if line.start_with? '+'
+          puts line.green
+        elsif line.start_with? '-'
+          puts line.red
+        else
+          puts line
+        end
+      end
+      print_separator
+    end
+
+    def print_file_info(diff)
+      unless diff.is_new?
+        puts "old file:"
+        puts "  path:       #{diff.old_filename}"
+        puts "  mode:       #{diff.old_mode}"
+        puts "  start line: #{diff.old_start_line}"
+        puts "  line count: #{diff.old_line_count}"
+      end
+
+      puts "new file:"
+      puts "  path:       #{diff.new_filename}"
+      puts "  mode:       #{diff.new_mode}"
+      puts "  start line: #{diff.new_start_line}"
+      puts "  line count: #{diff.new_line_count}"
+    end
+
+    def print_separator
+      separator = "=" * 100
+      puts separator
     end
   end
 end
